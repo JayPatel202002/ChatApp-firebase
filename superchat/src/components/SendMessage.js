3fa5b36799509c7c83b956b1a82cbaa8cbe1b520
JayPatel202002 <jp1006201@gmail.com>
2023-04-13 08:46:18 -0400

    .

--- superchat/src/components/SendMessage.js
+++ superchat/src/components/SendMessage.js
@@ -1,44 +1,43 @@
-import React, { useState } from "react";
-import { auth, db } from "../firebase";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  limit,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import Message from "./Message";
+import SendMessage from "./SendMessage";
 
-const SendMessage = ({ scroll }) => {
-  const [message, setMessage] = useState("");
-
-  const sendMessage = async (event) => {
-    event.preventDefault();
-    if (message.trim() === "") {
-      alert("Enter valid message");
-      return;
-    }
-    const { uid, displayName, photoURL } = auth.currentUser;
-    await addDoc(collection(db, "messages"), {
-      text: message,
-      name: displayName,
-      avatar: photoURL,
-      createdAt: serverTimestamp(),
-      uid,
+const ChatBox = () => {
+  const [messages, setMessages] = useState([]);
+  
+  useEffect(() => {
+    const q = query(
+      collection(db, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
     });
-    setMessage("");
-    scroll.current.scrollIntoView({ behavior: "smooth" });
-  };
+    return () => unsubscribe;
+  }, []);
+
   return (
-    <form onSubmit={(event) => sendMessage(event)} className="send-message">
-      <label htmlFor="messageInput" hidden>
-        Enter Message
-      </label>
-      <input
-        id="messageInput"
-        name="messageInput"
-        type="text"
-        className="form-input__input"
-        placeholder="type message..."
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit">Send</button>
-    </form>
+    <main className="chat-box">
+      <div className="messages-wrapper">
+        {messages?.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
+      <SendMessage />
+    </main>
   );
 };
-
-export default SendMessage;
+export default ChatBox;
