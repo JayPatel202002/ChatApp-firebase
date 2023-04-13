7565198263060f93e955ff3b6fa3e486b231cd27
JayPatel202002 <jp1006201@gmail.com>
2023-04-13 07:49:44 -0400

    add the auth function

--- superchat/src/App.js
+++ superchat/src/App.js
@@ -1,61 +1,19 @@
 import './App.css';
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-
-import { useAuthState } from "react-firebase-hooks";
-import { useCollectionData } from "react-firebase-hooks";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAhbWsJVQRAkfEsuvsQTYeZZZEHizYVPNI",
-  authDomain: "superchat-90924.firebaseapp.com",
-  projectId: "superchat-90924",
-  storageBucket: "superchat-90924.appspot.com",
-  messagingSenderId: "258867603184",
-  appId: "1:258867603184:web:b68aeb9926fd4dfb000435",
-  measurementId: "G-MYHL2H0WM6"
-};
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
-const [user] = useAuthState(auth);
-
+import NavBar from "./components/NavBar";
+import Welcome from "./components/Welcome";
+import ChatBox from "./components/ChatBox";
+import { useState } from "react";
+import { auth } from "./firebase";
+import { useAuthState } from 'react-firebase-hooks/auth';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-      <section>
-        {user ? <Chatroom /> : <SignIn /> }
-      </section>
+
+const user = useAuthState(auth);
+  return(
+  <div className="App">
+      <NavBar />
+      {!user ? <Welcome /> : <ChatBox />}
     </div>
   );
 }
-
-function SignIn(){
-const signinWithGoogle = () =>{
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider);
-
-}
-
-  return (
-    <button onClick = {signinWithGoogle}>
-      Sign in with google
-    </button>
-  ) 
-}
-
-function SignOut(){
-  return auth.currentUser && (
-    <button onClic = {()=> auth.signOut()}>
-      Sign out
-    </button>
-  )
-}
-
-function Chatroom() {}
 
 export default App;
---
+++ superchat/src/components/Welcome.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+const Welcome = () => {
+  const googleSignIn = () => {
+  };
+
+  return (
+    <main className="welcome">
+      <h2>Welcome to React Chat.</h2>
+      <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
+      <p>Sign in with Google to chat with with your fellow React Developers.</p>
+      <button className="sign-in">
+        <img
+          onClick={googleSignIn}
+          alt="sign in with google"
+          type="button"
+        />
+      </button>
+    </main>
+  );
+};
+
+export default Welcome;
